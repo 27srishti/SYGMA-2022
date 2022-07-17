70ff1a7e7de397faa36deb0327aa8cae079e3434
unknown <heyskanda@310>
2022-07-17 23:56:38 +0530

    fixed error

--- pages/index.js
+++ pages/index.js
@@ -49,7 +49,7 @@ function Nav() {
                       href="#Faq"
                       className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                     >
-                      FAQ's
+                      FAQ`s
                     </Link>{" "}
                     <Link
                       href="#Footer"
@@ -144,7 +144,7 @@ function Nav() {
                   href="#Faq"
                   className="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
                 >
-                  FAQ's
+                  FAQ`s
                 </a>
 
                 <a
